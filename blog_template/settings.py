6fc1a29b75fb6fd06d31c881033ceefdbee32278
Igor Aragão <roginaldosemog@gmail.com>
2020-08-19 16:08:27 -0300

    Add blog frontend simple model

--- blog_template/settings.py
+++ blog_template/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'blog_template.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
