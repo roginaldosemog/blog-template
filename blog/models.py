c52ae0e3684b10cecd0b382b143c3798c8057ee4
Igor Aragão <roginaldosemog@gmail.com>
2020-08-19 22:28:28 -0300

    Add category class to blog models

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,18 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__ (self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     summary = RichTextField()
     content = RichTextUploadingField()
     author = models.ForeignKey(User, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
 
     def __str__ (self):
