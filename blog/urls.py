64437d66f3e4fbe5fdf33c572c53d30cf8de587e
Igor Aragão <roginaldosemog@gmail.com>
2020-08-21 18:35:43 -0300

    Add 'UpdateViewForm'

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, ArticleView, AddArticleView
+from .views import HomeView, ArticleView, AddArticleView, UpdateArticleView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleView.as_view(), name="article"),
     path('article/new/', AddArticleView.as_view(), name="add_article"),
+    path('article/edit/<int:pk>', UpdateArticleView.as_view(), name="update_article"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Article
 from .forms import ArticleForm
 
@@ -15,3 +15,8 @@ class AddArticleView(CreateView):
     model = Article
     form_class = ArticleForm
     template_name = 'add_article.html'
+
+class UpdateArticleView(UpdateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'update_article.html'
