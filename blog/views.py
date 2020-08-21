2340515129720d805ad33824ffb673750d9f1ae8
Igor Aragão <roginaldosemog@gmail.com>
2020-08-21 19:04:16 -0300

    Add 'DeleteArticleView

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Article
 from .forms import ArticleForm
+from django.urls import reverse_lazy
 
 class HomeView(ListView):
     model = Article
     template_name = 'home.html'
+    ordering = ['-id']
 
 class ArticleView(DetailView):
     model = Article
@@ -20,3 +22,8 @@ class UpdateArticleView(UpdateView):
     model = Article
     form_class = ArticleForm
     template_name = 'update_article.html'
+
+class DeleteArticleView(DeleteView):
+    model = Article
+    template_name = 'delete_article.html'
+    success_url = reverse_lazy('home')
